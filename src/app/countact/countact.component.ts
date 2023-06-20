c9105de6e5f2163fc994389cdcd337cafcfd2ac6
ndeanka <williammlula4@gmail.com>
2023-06-20 23:25:06 +0300

    general changes

--- src/app/countact/countact.component.ts
+++ src/app/countact/countact.component.ts
@@ -1,10 +1,41 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ContactService } from '../service/contact.service';
+import { Contact } from '../share/contact';
 
 @Component({
   selector: 'app-countact',
   templateUrl: './countact.component.html',
   styleUrls: ['./countact.component.css']
 })
-export class CountactComponent {
+export class CountactComponent implements OnInit{
+  contactArr: Contact[] = [];
+  newContact!: Contact;
+  constructor(private contactService: ContactService){}
 
+  ngOnInit(): void {
+    this.getContact();
+    this.addContact();
+      
+  }
+  getContact(){
+    this.contactService.getContacts().subscribe(
+      (contact: Contact[]) => {
+        this.contactArr = contact
+      },
+      (err: any) => {
+        console.log('Unable to get list of tasks:', err);
+      }
+    )
+  }
+
+  addContact(){
+    this.contactService.addContact(this.newContact).subscribe(
+      (contact: Contact) => {
+        this.contactArr.push(contact);
+      },
+      (err: any) => {
+        console.log('Unable to add contact:', err);
+      } 
+    )
+  }
 }
